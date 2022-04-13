40ad136d299085dc9aa9501467f8ba5af27e2ec5
Djody Kort <507946@vistacollege.nl>
2022-04-13 13:54:25 +0200

    Version 1.0.1
    Made the homepage
    
    To Do List:
    Projects Page

---
+++ html/nl/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang='nl'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+        <link rel='stylesheet' href='../../css/style.css'>
+
+        <title>Code Portofolio Djody Kort</title>
+    </head>
+    <body class='index-body'>
+        <div class='navbar'>
+            <a href='../../index.php'><img  src='../../images/EN_Flag.svg' alt='Look for the NLFlag.jpeg'></a>
+        </div>
+
+        <div class='index-container'>
+            <h1>Hey, I'm Djody</h1>
+            <h2>Full-Time Sofware Developer Student.</h2>
+
+            <div class='index-container2'>
+                <p class='float-left'>
+                    Dit is mijn code portofolio. Hier kun je al mijn projecten vinden<br/>
+                    De projecten zijn school en persoonlijk gerelateerd<br/>
+                    Ik heb veel projecten gemaakt binnnen mijn opleiding en ik hoop dat je ze leuk vindt<br/>
+                </p>
+                <p class='float-right'>
+                    Eerder gewerkt met C#,<br/>
+                    HTML, CSS, JavaScript, PHP, SQL,<br/>
+                    Python and Unity.
+                </p>
+            </div>
+        </div>
+        <!-- Footer with links to Github and Project page -->
+        <div class='footer'>
+            <a href='https://github.com/DjodyKort' class='footer-link'>
+                <img src='../../images/github-logo.png' alt='Look for the Github.png' class='footer-img'>
+            </a>
+            <div>
+                <a href='../nl/projects.php' class='footer-link'>
+                    <img src='../../images/projects-lamp.png' alt='Look for the Project.png' class='footer-img'>
+                </a>
+                <p>Projecten</p>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+        <link rel='stylesheet' href='./css/style.css'>
+
+        <title>Code Portofolio Djody Kort</title>
+    </head>
+    <body class='index-body'>
+        <div class='navbar'>
+            <a href='./html/nl/index.php'><img  src='./images/NL_Flag.png' alt='Look for the NLFlag.jpeg'></a>
+        </div>
+
+        <div class='index-container'>
+            <h1>Hey, I'm Djody</h1>
+            <h2>Full-Time Sofware Developer Student.</h2>
+
+            <div class='index-container2'>
+                <p class='float-left'>
+                    This is my code portofolio. You can find all my projects here<br/>
+                    The projects are school and personally related.<br/>
+                    I have made a lot of projects in my school and I hope you will like them.<br/>
+                </p>
+                <p class='float-right'>
+                    Previously worked with C#,<br/>
+                    HTML, CSS, JavaScript, PHP, SQL,<br/>
+                    Python and Unity.
+                </p>
+            </div>
+        </div>
+        <!-- Footer with links to Github and Project page -->
+        <div class='footer'>
+            <a href='https://github.com/DjodyKort' class='footer-link'>
+                <img src='./images/github-logo.png' alt='Look for the Github.png' class='footer-img'>
+            </a>
+            <div>
+                <a href='./html/en/projects.php' class='footer-link'>
+                    <img src='./images/projects-lamp.png' alt='Look for the Project.png' class='footer-img'>
+                </a>
+                <p>Projects</p>
+            </div>
+        </div>
+    </body>
+</html>
